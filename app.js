1ba54507452f4c076dd28735ae44a8850d0da1be
jason <jmasters1000@yahoo.com>
2021-04-18 21:22:18 -0700

    Fix app.js

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const db = require('./config/keys').mongoURI;
 const User = require('./models/User');
 const passport = require('passport');
 require('./config/passport')(passport);
+const path = require('path')
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
